783f3e1f52adf4ee9384902340c4efcb7065b0f6
thebrettd <thebrettd@gmail.com>
2014-03-31 14:45:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBlackJack::Application.config.secret_key_base = '24014d7e2f74002cfc0335119377be865cbb83b1a923f37a8d5691f00a335c9fed9f4d38bb659bb2b82b28a94e458c591c6f88bf3368bec38e7d6a67d4d77711'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsBlackJack::Application.config.session_store :cookie_store, key: '_RailsBlackJack_session'
